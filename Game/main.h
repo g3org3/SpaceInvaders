7f4104128399668da412b275aa906de8026b3dd5
George <7jagjag@gmail.com>
2014-02-06 10:43:52 -0600

    YA FUNCIONAL

--- Game/main.h
+++ Game/main.h
@@ -25,6 +25,10 @@ void setStruct(struct objeto *o, float posx, float posy, float height, float wid
     o-> self = al_create_bitmap(width, height);
 }
 
+void setStructImg(struct objeto *o, ALLEGRO_BITMAP *b){
+    o-> self = b;
+}
+
 void setStructColor(ALLEGRO_DISPLAY *display, struct objeto *o, int c){
     al_set_target_bitmap(o->self);
     switch (c) {
@@ -53,30 +57,41 @@ void structMove(struct objeto *o, float w){
     o->y += o-> dy;
 }
 
+void structMoveAll(struct objeto *o[], float w, int size){
+    for (int i=0; i<size; i++) {
+        if(o[i]->x>=w)
+            o[i]->dx *= -1;
+        if(o[i]->x<=0)
+            o[i]->dx *= -1;
+        o[i]->x += o[i]-> dx;
+        o[i]->y += o[i]-> dy;
+    }
+}
+
 bool checkCollition(struct objeto *o, struct objeto *b){
     bool rango1 = b->x >= o->x;
     bool rango2 = b->x <= o->x + o->w;
     bool rangoX = rango1 && rango2;
     
-    //bool rangoA = b->y >= o->y;
-    //bool rangoB = b->y <= o->y + o->h;
-    //bool rangoY = rangoA && rangoB;
+    bool rangoA = b->y >= o->y;
+    bool rangoB = b->y <= o->y + o->h;
+    bool rangoY = rangoA && rangoB;
     
-    bool rangoY = o->y+o->h == b->y;
+    //bool rangoY = o->y+o->h == b->y;
     
     return rangoX && rangoY;
 }
 
 
 void createBala(struct objeto *balas[], ALLEGRO_DISPLAY *display, struct objeto *nave, int size){
-    for (int i=1; i<size; i++) {
+    for (int i=0; i<size; i++) {
         if(balas[i]==0){
             balas[i] = (struct objeto *) malloc(sizeof(struct objeto));
-            setStruct(balas[i], 0, 0, 50, 10, 0, -5);
+            setStruct(balas[i], 0, 0, 10, 10, 0, -6);
             setStructColor(display, balas[i], 2);
             (*balas[i]).x = nave->x + nave->w/2;
             (*balas[i]).y = nave->y;
-            printf("%d\n", i);
+            //printf("%d\n", i);
             i = size*size;
             
         }
@@ -85,12 +100,13 @@ void createBala(struct objeto *balas[], ALLEGRO_DISPLAY *display, struct objeto 
 
 bool drawBala(struct objeto *balas[], int size, struct objeto *enemigo){
     bool choco;
-    for (int i=1; i<size; i++) {
+    for (int i=0; i<size; i++) {
         if(balas[i]!=0){
             choco = checkCollition(enemigo, balas[i]);
             if(!choco){
                 al_draw_bitmap(balas[i]->self, balas[i]->x, balas[i]->y, 0);
                 balas[i]->y += balas[i]->dy;
+                //printf("choco");
                 
             } else{
                 enemigo->x = -512;
@@ -101,9 +117,32 @@ bool drawBala(struct objeto *balas[], int size, struct objeto *enemigo){
     al_draw_bitmap(enemigo->self, enemigo->x, enemigo->y, 0);
     return false;
 }
+bool drawBalas(struct objeto *balas[], int size, struct objeto *enemigos[], int length){
+    bool choco;
+    for (int i=0; i<size; i++) {
+        if(balas[i]!=0){
+            for (int j=0; j<length; j++) {
+                choco = checkCollition(enemigos[j], balas[i]);
+                if(!choco){
+                    al_draw_bitmap(balas[i]->self, balas[i]->x, balas[i]->y, 0);
+                    balas[i]->y += balas[i]->dy;
+                    
+                } else{
+                    enemigos[j]->y = -512;
+                    balas[i]->x = -512;
+                }
+                //printf("%d", choco);
+            }
+        }
+    }
+    for (int i=0; i<length; i++) {
+        al_draw_bitmap(enemigos[i]->self, enemigos[i]->x, enemigos[i]->y, 0);
+    }
+    return false;
+}
 
 void deleteBala(struct objeto *balas[], int size){
-    for (int i=1; i<size; i++) {
+    for (int i=0; i<size; i++) {
         if(balas[i]!=0){
             if((*balas[i]).y < 0-balas[i]->h){
                 free(balas[i]);
@@ -113,9 +152,21 @@ void deleteBala(struct objeto *balas[], int size){
     }
 }
 
+void createEnemies(struct objeto *o[], int size, ALLEGRO_DISPLAY *display){
+    for (int i=0; i<size; i++) {
+        o[i] = (struct objeto *) malloc(sizeof(struct objeto));
+        setStruct(o[i], 100+(200*i), 30+(80*i), 70, 100, 5+(i*2), 0);
+        setStructColor(display, o[i], 3);
+    }
 
+}
 
-
+void setEnemiesImg(struct objeto *o[], int size, ALLEGRO_BITMAP *b){
+    for (int i=0; i<size; i++) {
+        o[i]->self = b;
+    }
+    
+}
 
 
 
--- Game/main.m
+++ Game/main.m
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
 #include "main.h"
 
-#define SCREEN_W 800
-#define SCREEN_H 500
-#define FPS 60.00
+#define SCREEN_W 720
+#define SCREEN_H 480
+#define FPS 30.00
+
+#define B_SIZE 3
+#define E_SIZE 3
 
 int main(){
     ALLEGRO_DISPLAY *display;
     ALLEGRO_TIMER *timer;
     ALLEGRO_EVENT_QUEUE *event_queue;
     ALLEGRO_KEYBOARD_STATE keystate;
+    ALLEGRO_BITMAP *bg;
     
-    bool done, drawbala, creating;
+    
+    bool done, drawbala;
     int x = 4;
     drawbala = false;
     
     al_init();
     al_install_keyboard();
+    al_init_image_addon();
+    
     
     display = al_create_display(SCREEN_W, SCREEN_H);
     event_queue = al_create_event_queue();
@@ -26,21 +34,24 @@ int main(){
     
     // create space
     struct objeto nave;
-    setStruct(&nave, SCREEN_W/2, SCREEN_H-30, 100, 100, 4, 0);
+    setStruct(&nave, SCREEN_W/2, SCREEN_H-50, 40, 70, 10, 0);
     setStructColor(display, &nave, 1);
+    setStructImg(&nave, al_load_bitmap("ship.png"));
     // end
     
     // create bala
-    struct objeto *balas[4];
-    for (int i=0; i<4; i++) {
+    struct objeto *balas[B_SIZE];
+    for (int i=0; i<B_SIZE; i++) {
         balas[i] = 0000000;
     }
     // end
     
-    // create enemy
-    struct objeto enemigo;
-    setStruct(&enemigo, 200, 80, 70, 100, 5, 0);
-    setStructColor(display, &enemigo, 3);
+    bg = al_load_bitmap("Deep_Space.png");
+    
+    // create enemies
+    struct objeto *enemigos[E_SIZE];
+    createEnemies(enemigos, E_SIZE, display);
+    setEnemiesImg(enemigos, E_SIZE, al_load_bitmap("enemigo.png"));
     // end
     
     al_register_event_source(event_queue, al_get_keyboard_event_source());
@@ -62,28 +73,29 @@ int main(){
         }
         if(events.type == ALLEGRO_EVENT_TIMER){
             if(al_key_down(&keystate, ALLEGRO_KEY_RIGHT))
-                nave.x += nave.dx;
+                if(nave.x+nave.w<SCREEN_W)
+                    nave.x += nave.dx;
             if(al_key_down(&keystate, ALLEGRO_KEY_LEFT))
                 nave.x -= nave.dx;
             
             if(al_key_down(&keystate, ALLEGRO_KEY_SPACE)){
                 if(x==4){
-                    printf("space%f\n", enemigo.x);
-                    createBala(balas, display, &nave, 4);
+                    createBala(balas, display, &nave, B_SIZE);
                     x=0;
                 }
                 if(x<4)
                     x++;
             }
             
-            drawBala(balas, 4, &enemigo);
-            deleteBala(balas, 4);
+            drawBalas(balas, B_SIZE, enemigos, E_SIZE);
+            deleteBala(balas, B_SIZE);
             //printf("%p, %p, %p, %p\n", balas[0], balas[1], balas[2], balas[3]);
             //al_draw_bitmap(enemigo.self, enemigo.x, enemigo.y, 0);
             al_draw_bitmap(nave.self, nave.x, nave.y, 0);
             al_flip_display();
             al_clear_to_color(al_map_rgb(0, 0, 0));
-            structMove(&enemigo, SCREEN_W);
+            al_draw_bitmap(bg, 0, 0, 0);
+            structMoveAll(enemigos, SCREEN_W, E_SIZE);
             
         }
         
